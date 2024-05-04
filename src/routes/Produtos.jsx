373cced397fe3b4e3c96b2f424a6a865f9eedf27
Marcelo <marcelovieira048@gmail.com>
2024-05-04 17:59:56 -0300

    feat: logout added

--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -1,7 +1,24 @@
+import {} from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function Produtos() {
+
+    //UTILIZANDO O useNavigate para redirecionar componentes
+    const navigate= useNavigate();
+
+    //CRIANDO A FUNCAO HANDLELOGOUT
+    const handleLogout =()=>{
+      sessionStorage.removeItem("usuario");
+      sessionStorage.removeItem("senha");
+      alert("saindo do sistema...")
+      navigate("/");
+    }
+    
     return (
       <>
+
+      <button onClick={handleLogout}>Logout</button>
+      
       <div className="container bg-image">
         <div className="produtos">
           <h1>Produtos</h1>
