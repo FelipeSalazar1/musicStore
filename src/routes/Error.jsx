fc001d16aeff6d15079461de1a92fa5a04beae70
Felipe <felipe.salazar2004@outlook.com>
2024-05-05 01:28:49 -0300

    Feat: Error Page

--- src/routes/Error.jsx
+++ src/routes/Error.jsx
@@ -1,7 +1,18 @@
+import {} from "react";
+import { Link } from "react-router-dom";
+import '../Scss/Error.scss' 
+import errorImg from '../assets/errorImg.svg' 
+
 export default function Error() {
     return (
       <>
-      <h1>404-Página não encontrada !!!</h1>
+      <section className="format-page">
+        <img src={errorImg} alt="errorMsg" />
+        <h1>404 - PAGE NOT FOUND... PLEASE TRY AGAIN!</h1>
+        <Link to="/" className="return-btn">
+          RETURN TO HOME
+        </Link>
+      </section>
       </>
     );
   }
