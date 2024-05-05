d8b7a0b2af8e954451e1e4501ebb5655edb495fc
Felipe <felipe.salazar2004@outlook.com>
2024-05-05 01:16:18 -0300

    Feat: Contact Page

--- src/routes/Contato.jsx
+++ src/routes/Contato.jsx
@@ -1,7 +1,56 @@
+import '../Scss/Contato.scss'
+
 export default function Contato() {
     return (
       <>
-      <h1>CONTATOS</h1>
+      <section className="contact">
+        <h1 className="form-title">CONTATE-NOS</h1>
+
+        <form className="forms">
+          <div className="form-group">
+            <input
+              id="name"
+              required
+              placeholder="Nome Completo*"
+              className="form-input"
+              type="text"
+            />
+          </div>
+
+          <div className="form-group">
+            <input
+              id="email"
+              required
+              placeholder="E-mail*"
+              className="form-input"
+              name="email"
+              type="email"
+            />
+          </div>
+
+          <div className="form-group">
+            <textarea
+              required
+              placeholder="Mensagem*"
+              className="form-input"
+              name="message"
+              id="message"
+            ></textarea>
+          </div>
+
+          <div id="check">
+            <input className="checkbox" type="checkbox" id="checkbox" />
+            <label id="info-check">
+              Concordo com o envio dos dados e autorizo a Central Music a entrar
+              contato comigo através deles.
+            </label>
+          </div>
+
+          <button className="form-button" type="submit">
+            ENVIAR MENSAGEM
+          </button>
+        </form>
+      </section>
       </>
     );
   }
