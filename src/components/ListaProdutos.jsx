b85f955a47e45b7028ac78a0db8e4ed63c7b158a
Felipe <felipe.salazar2004@outlook.com>
2024-05-05 22:11:41 -0300

    Fix: remove position absolute

---
+++ src/components/ListaProdutos.jsx
@@ -0,0 +1,11 @@
+export const ListaProdutos =[
+  {id:1, nome:"Guitarra Amarela",descricao:"Guitarra Les Paul Amarela", valor: 3400},
+  {id:2, nome:"Guitarra Vermelha",descricao:"Guitarra StratoCaster Vermelha", valor: 2200},
+  {id:3, nome:"Guitarra Marrom",descricao:"Guitarra StratoCaster Marrom", valor: 2700},
+] 
+
+export const ListaProdutos2 =[
+  {id:1, nome:"Cabo Amarelo",descricao:"Cabo de Guitarra P10 Amarelo", valor: 3400},
+  {id:2, nome:"Cabo Verde",descricao:"Cabo de Guitarra P10 Verde", valor: 3400},
+  {id:1, nome:"Pedal BUFFER",descricao:"Pedal efeito Buffer Guitarra", valor: 3400},
+] 
